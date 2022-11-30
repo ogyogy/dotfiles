4d172f6575999406abf9cabd30050f7f2ed51c51
ogyogy <47146269+ogyogy@users.noreply.github.com>
2022-11-30 23:01:43 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+ln -sf ~/dotfiles/.bash_aliases ~/.bash_aliases
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.inputrc ~/.inputrc
 ln -sf ~/dotfiles/.zsh_aliases ~/.zsh_aliases
